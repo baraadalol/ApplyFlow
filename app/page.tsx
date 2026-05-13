f43e147bf817c48c6ec4ace6a5c25a71639a5aaf
Baraa Dalol <baraa.dalol@chasacademy.se>
2026-05-13 15:14:03 +0000

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>ApplyFlow</h1>
+      <p>LIA application tracker</p>
+
+      <ul>
+        <li>
+          <a href="/dashboard">Dashboard</a>
+        </li>
+
+        <li>
+          <a href="/companies">Companies</a>
+        </li>
+      </ul>
+    </main>
+  );
+}
